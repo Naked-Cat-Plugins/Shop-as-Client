6f204194d276555af6511ae2b63451b1bb5844c4
Sérgio Santos <me@s3rgiosan.com>
2024-02-18 02:06:58 +0000

    Rename class

--- shop-as-client.php
+++ shop-as-client.php
@@ -356,12 +356,12 @@ add_action( 'before_woocommerce_init', function() {
 
 add_action(
 	'woocommerce_blocks_loaded',
-	function() {
-		require_once __DIR__ . '/includes/shop-as-client-blocks-integration.php';
+	function () {
+		require_once __DIR__ . '/includes/class-shop-as-client-blocks-integration.php';
 
 		add_action(
 			'woocommerce_blocks_checkout_block_registration',
-			function( $integration_registry ) {
+			function ( $integration_registry ) {
 				$integration_registry->register( new ShopAsClient_Blocks_Integration() );
 			}
 		);
