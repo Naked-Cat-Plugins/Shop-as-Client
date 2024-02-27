a80344964ce71100ff8f76b1b7ac975e6f67485a
Sérgio Santos <me@s3rgiosan.com>
2024-02-27 23:07:55 +0000

    Add support for shop_as_client_update_customer_data filter

--- includes/class-shop-as-client-extend-store-endpoint.php
+++ includes/class-shop-as-client-extend-store-endpoint.php
@@ -180,6 +180,13 @@ class ShopAsClient_Extend_Store_Endpoint {
 		$order->set_customer_id( $user_id );
 		$order->save();
 
+		if ( apply_filters( 'shop_as_client_update_customer_data', false ) ) {
+			$customer      = new \WC_Customer( $user_id );
+			$customer_data = static::get_customer_data_by_order_id( $order->get_id() );
+			static::switch_customer_data( $customer, $customer_data );
+			$customer->save();
+		}
+
 		do_action( 'shop_as_client_checkout_order_processed', $order, $user_id );
 
 		static::restore_customer_data();
@@ -237,6 +244,42 @@ class ShopAsClient_Extend_Store_Endpoint {
 			'shipping_postcode'   => $customer->get_shipping_postcode(),
 			'shipping_country'    => $customer->get_shipping_country(),
 			'shipping_phone'      => $customer->get_shipping_phone(),
+		);
+
+		return $customer_data;
+	}
+
+	/**
+	 * Get customer data by order ID.
+	 *
+	 * @param  int $order_id The order ID.
+	 * @return array
+	 */
+	public static function get_customer_data_by_order_id( $order_id ) {
+		$order = new \WC_Order( $order_id );
+
+		$customer_data = array(
+			'billing_first_name'  => $order->get_billing_first_name(),
+			'billing_last_name'   => $order->get_billing_last_name(),
+			'billing_company'     => $order->get_billing_company(),
+			'billing_address_1'   => $order->get_billing_address_1(),
+			'billing_address_2'   => $order->get_billing_address_2(),
+			'billing_city'        => $order->get_billing_city(),
+			'billing_state'       => $order->get_billing_state(),
+			'billing_postcode'    => $order->get_billing_postcode(),
+			'billing_country'     => $order->get_billing_country(),
+			'billing_email'       => $order->get_billing_email(),
+			'billing_phone'       => $order->get_billing_phone(),
+			'shipping_first_name' => $order->get_shipping_first_name(),
+			'shipping_last_name'  => $order->get_shipping_last_name(),
+			'shipping_company'    => $order->get_shipping_company(),
+			'shipping_address_1'  => $order->get_shipping_address_1(),
+			'shipping_address_2'  => $order->get_shipping_address_2(),
+			'shipping_city'       => $order->get_shipping_city(),
+			'shipping_state'      => $order->get_shipping_state(),
+			'shipping_postcode'   => $order->get_shipping_postcode(),
+			'shipping_country'    => $order->get_shipping_country(),
+			'shipping_phone'      => $order->get_shipping_phone(),
 		);
 
 		return $customer_data;
